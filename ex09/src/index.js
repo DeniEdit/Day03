f60f39f9577e3452c8667e8c15de1c1383ee6ebb
DeniEdit <milicevic.deni@gmail.com>
2021-03-10 18:18:54 +0100

    The Subtract of Two Numbers

---
+++ ex09/src/index.js
@@ -0,0 +1,15 @@
+function main(numOne,numTwo){
+    // Only change code below this line
+    
+    difference= numOne - numTwo;
+    
+    // Only change code below this line
+    
+    return difference;
+}
+
+console.log(main(15,8)); // change this line
+console.log(main(25,8));
+console.log(main(425,120));
+
+module.exports = main;
