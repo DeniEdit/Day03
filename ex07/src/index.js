859c57c80de549b95e2164e2e61d16b478e3e6de
DeniEdit <milicevic.deni@gmail.com>
2021-03-10 17:58:46 +0100

    Case Sensitivity

---
+++ ex07/src/index.js
@@ -0,0 +1,26 @@
+function main(){
+    // Only change code below this line
+    //Variable declarations
+    var StUdLyCapVaR;
+    var properCamelCase;
+    var TitleCaseOver;
+
+    //Variable assignments
+    STUDLYCAPVAR=10;
+    PRoperCAmelCAse = "A String";
+    tITLEcASEoVER=9000;
+    
+    // Only change code below this line
+    studlyCapVar=10;
+    properCamelCase="A String";
+    titleCaseOver=9000;
+    
+    return{
+        studlyCapVar,
+        properCamelCase,
+        titleCaseOver
+    }
+}
+
+console.log(main());
+module.exports = main;
