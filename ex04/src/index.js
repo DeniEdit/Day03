58c4a34b112089f841cedeae0b33999b5fcfaacc
DeniEdit <milicevic.deni@gmail.com>
2021-03-10 17:37:13 +0100

    Assignment Operator

---
+++ ex04/src/index.js
@@ -0,0 +1,14 @@
+function main(){
+    // Only change code below this line
+
+    // Only change code below this line
+    var x =10;
+    var y;
+    y=x;
+
+    return [x,y];
+}
+
+console.log(main());
+
+module.exports= main;
