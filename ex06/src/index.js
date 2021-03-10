fe7f8e8ba226eba4d258d641b5392887be39cfa3
DeniEdit <milicevic.deni@gmail.com>
2021-03-10 17:49:27 +0100

    Uninitialized Variables

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,10 +1,15 @@
 function main(){
     // Only change code below this line
+    var x;
+    var y;
+    var z;
 
+    x=7, y=12, z="My name is ";
     // Only change code below this line
-    var x =8;
-
-    return x;
+    x = x +2;
+    y = y + 4;
+    z = z + "John!";
+    return [x,y,z];
 }
 
 console.log(main());
